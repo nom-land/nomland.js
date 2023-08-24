b4aad6a100227246e2935a615d1ed7db35df1b2b
Saltad <saltad@proton.me>
2023-08-25 00:48:53 +0800

    feat: draft for the sdk

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nomland SDK
+
+This is the SDK interacting with Nomland protocol.
+
+It can be run in node environment or browser. Typically in node environment, curators are kept under the admin address. Whereas in browser environment, curators are kept under the user's address. The key difference is to initiliaze Nomland via a private key or provider.
+
+Currently it is suggested to use node environment. Browser version is unstable.
---
+++ src/utils/index.ts
@@ -0,0 +1,50 @@
+import { settings } from "../config";
+
+import md5 from "md5";
+// const punycode = require("punycode/");
+import punycode from "punycode/";
+
+export function hashOf(content: string, digits = 4, suffix = true): string {
+    const hash = md5(content);
+    if (suffix) return hash.slice(hash.length - digits, hash.length);
+    else return hash.slice(0, digits);
+}
+
+export function encode(content: string) {
+    let str = punycode.encode(content);
+    // remove " " and "\n"
+    str = str.replace(/[\s\n]/g, "");
+    // if char is not in [a-z0-9-_], replace it with "-"
+    str = str.replace(/[^a-z0-9-_]/g, "-");
+    // remove all "-" at the beginning
+    str = str.replace(/^-+/g, "");
+    return str;
+}
+
+export function feedbackUrl(
+    cid: string,
+    rid: string,
+    curatorId: string,
+    noteId: string
+) {
+    if (settings.botConfig.prod === false) {
+        // return `https://colib-home.vercel.app/community/${cid}/record/${rid}`;
+        return `https://colib-home.vercel.app/curation/${curatorId}-${noteId}`;
+    } else {
+        return `https://colib.app/curation/${curatorId}-${noteId}`;
+    }
+}
+
+const appPrefix = settings.appName.slice(0, 2);
+
+export function makeListLinkType(list: string) {
+    return `${appPrefix}-ls-${list}`;
+}
+
+export function getListLinkTypePrefix() {
+    return `${appPrefix}-ls-`;
+}
+
+export function getMembersLinkType() {
+    return `${appPrefix}-members`;
+}
